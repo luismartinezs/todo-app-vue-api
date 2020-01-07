51aee08cabe14175c6f4bb41790132a54d0c28df
luismartinezs <martinez.s.luis@gmail.com>
2020-01-07 11:54:54 +0100

    Clean up comments

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ const morgan = require('morgan') // Logging
 
 const toDo = require('./controller/to-do')
 
-// const user = process.env.DB_USER
-// const password = process.env.DB_PASSWORD
-// const host = process.env.DB_HOST
-// const port = process.env.DB_PORT
-// const database = process.env.DB_DATABASE
-
-// const connectionString = `postgresql://${user}:${password}@${host}:${port}/${database}`
-
 const db = knex({
   client: 'pg',
   connection: process.env.DB_URI
